32d63f392741eabc8d7b42ffb5da59cc59654738
Josh XT <josh@devxt.com>
2023-05-18 16:12:26 -0400

    Add readme

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup, find_packages
+import os
+
+# Read the contents of your README file
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name="simple_auth",
     version="0.0.1",
     description="A small useful Python module to abstract away the common auth header methods used by different software vendors. The output is a requests session with the authentication headers built in.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Josh XT",
     author_email="josh@devxt.com",
     package_dir={"": "src"},
