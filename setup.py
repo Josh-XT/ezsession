45e6e405a2cabd0db26d9004fafb78185e980e23
Josh XT <josh@devxt.com>
2023-05-18 16:06:26 -0400

    Setup python package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="auth_headers",
+    version="0.0.1",
+    description="A small useful Python module to abstract away the common auth header methods used by different software vendors. The output is a requests session with the authentication headers built in.",
+    author="Josh XT",
+    author_email="josh@devxt.com",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+)
